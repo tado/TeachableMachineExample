d1979315653eae74b8f6bafa34b46db1d0ae7401
Atsushi Tadokoro <tadokoro@gmail.com>
2024-10-10 09:47:25 +0900

    Update sketch.js

--- 00_BasicTemplate/sketch.js
+++ 00_BasicTemplate/sketch.js
@@ -8,60 +8,49 @@ p5.js + ml5.jsを使ったウェブカメラ画像の分類。
 // !!ここに自分のモデルのURLを入力!!
 let imageModelURL = 'https://teachablemachine.withgoogle.com/models/8dyZwOgL4/';
 
-// Classifier変数
+// クラス分類器の変数
 let classifier;
 // ビデオ
 let video;
-let flippedVideo;
-// 分類結果のラベルを格納
+// クラス分類の結果を保存するための変数
 let label = "";
 
-// モデルをプリロード
+// 最初にモデルを読み込む
 function preload() {
-  classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+  classifier = ml5.imageClassifier(imageModelURL + "model.json", {
+    flipped: true,
+  });
 }
 
 function setup() {
-  createCanvas(640, 500);
-  // ビデオを設定
-  video = createCapture(VIDEO);
-  video.size(640, 480);
+  createCanvas(320, 260);
+  // ビデオを作成
+  video = createCapture(VIDEO, { flipped: true });
+  video.size(320, 240);
   video.hide();
-  flippedVideo = ml5.flipImage(video);
-  // 分類処理を開始
-  classifyVideo();
+  classifier.classifyStart(video, gotResult);
 }
 
 function draw() {
   background(0);
   // ビデオを描画
-  image(flippedVideo, 0, 0);
-  // 分類結果のラベルを描画
+  image(video, 0, 0);
+
+  // ラベルを描画
   fill(255);
   textSize(16);
   textAlign(CENTER);
   text(label, width / 2, height - 4);
 }
 
-// 現在のビデオ画像からラベルを予測
+// 現在のビデオフレームを分類する
 function classifyVideo() {
-  flippedVideo = ml5.flipImage(video)
   classifier.classify(flippedVideo, gotResult);
 }
 
-// 分類結果を取得
-function gotResult(error, results) {
-  // エラー処理
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  // 結果をコンソールに表示
-  console.log(results[0]);
-  
-  // 分類結果のラベルを取得
+// 結果が得られたとき
+function gotResult(results) {
+  // 結果は信頼度順に配列に格納される
+  // console.log(results[0]);
   label = results[0].label;
-  // 再度分類処理を行う
-  classifyVideo();
 }
